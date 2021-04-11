5011cf7521387b971ba2a9cdfad28099fcd4528c
rebintex <rebintex@mail.ru>
2021-04-11 18:38:44 +0500

    Hamburger menu is installed

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// let mobileMenu = document.querySelector('.mobile-menu');
+// let activeMenu = document.querySelector('.active-header__nav');
+
+// mobileMenu.addEventListener('click', function() {
+    
+//    activeMenu.classList.toggle('showMenu');
+   
+// }) 
+let mobileMenu = document.querySelector('.mobile-menu');
+let activeMenu = document.querySelector('.active-header__nav');
+
+function showMenu () {
+  
+   activeMenu.classList.toggle('showMenu');
+   console.log(mobileMenu)
+  
+}
